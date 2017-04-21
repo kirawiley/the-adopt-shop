09c4d015fb9889de5589de09bd5de089ba826e3d
Kira Wiley <kaysimone5@gmail.com>
2017-04-21 14:07:12 -0700

    Seed file populates database.

--- findPets.js
+++ findPets.js
@@ -1,5 +1,5 @@
-module.exports = function findPets(type, breed, gender, pets) {
-  knex('pets')
+module.exports = function findPets(type, breed, gender, database) {
+  database
     .where('type', type)
     .where('breed', breed)
     .where('gender', gender)
